a9a3957152cc193f45e869ae5ed33ff46f0364e1
Hakan Baştürk <forxynet@gmail.com>
2021-11-10 16:20:31 +0300

    DemoCode and DemoCode.Tests updated

--- DemoCode/FlightDetails.cs
+++ DemoCode/FlightDetails.cs
@@ -1,47 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace DemoCode
 {
     public class FlightDetails
     {
-        private string _arrivalAirportCode;
-        private string _departureAirportCode;
-
-        public string DepartureAirportCode
+        public FlightDetails(AirportCode departureAirportCode, AirportCode arrivalAirportCode)
         {
-            get => _departureAirportCode;
-            set
-            {
-                EnsureValidAirportCode(value);
-                _departureAirportCode = value;
-            }
+            DepartureAirportCode = departureAirportCode;
+            ArrivalAirportCode = arrivalAirportCode;
         }
 
-        public string ArrivalAirportCode
-        {
-            get => _arrivalAirportCode;
-            set
-            {
-                EnsureValidAirportCode(value);
-                _arrivalAirportCode = value;
-            }
-        }
-
+        public AirportCode DepartureAirportCode { get; }
+        public AirportCode ArrivalAirportCode { get; }        
         public TimeSpan FlightDuration { get; set; }
         public string AirlineName { get; set; }
-        public List<string> MealOptions { get; set; } = new List<string>();
-
-        private void EnsureValidAirportCode(string airportCode)
-        {
-            var isWrongLength = airportCode.Length != 3;
-
-            var isWrongCase = airportCode != airportCode.ToUpperInvariant();
-
-            if (isWrongLength || isWrongCase)
-            {
-                throw new Exception(airportCode + " is an invalid airport");
-            }
-        }
     }
 }
