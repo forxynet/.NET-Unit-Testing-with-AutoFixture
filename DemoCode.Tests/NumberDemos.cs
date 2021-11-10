0e3d6ff0d148d6aaf04ef3ea7f4bdf130fcc3b69
Hakan Baştürk <forxynet@gmail.com>
2021-11-10 12:44:10 +0300

    DemoCode updated

---
+++ DemoCode.Tests/NumberDemos.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using AutoFixture;
+
+namespace DemoCode.Tests {
+    public class NumberDemos {
+
+        [Fact]
+        public void Inits() {
+            var sut = new IntCalculator();
+            var fixture = new Fixture();
+            var anonymousNember = fixture.Create<int>();
+
+            sut.Subtract(anonymousNember);
+            Assert.True(sut.Value < 0);
+        }
+    }
+}
